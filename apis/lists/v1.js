8f033dc2a74c8d1abb4cf63e039891c8eb8875ee
Simon Mayes <simon@msyea.co.uk>
2015-01-27 23:17:24 +0000

    Introducing lists and refactoring people

---
+++ apis/lists/v1.js
@@ -0,0 +1,56 @@
+var Api = require('../../lib/api'),
+  createAPIRequest = require('../../lib/apirequest');
+var util = require('util');
+
+function Lists(options) {
+  Lists.super_.call(this);
+
+  var self = this;
+  this._options = options || {};
+  /**
+   * people.list
+   * @param  {object}   params   Parameters for the request
+   * @param  {callback} callback The callback that handles the response
+   * @return {object}            Request object
+   */
+  this.list = function(params, callback) {
+    var parameters = {
+      options: {
+        url: 'https://{slug}.nationbuilder.com/api/v1/lists',
+        method: 'GET'
+      },
+      params: params,
+      requiredParams: ['slug'],
+      pathParams: ['slug'],
+      context: self
+    };
+
+    return createAPIRequest(parameters, callback);
+  };
+
+  this.people = function(params, callback) {
+    var parameters = {
+      options: {
+        url: 'https://{slug}.nationbuilder.com/api/v1/lists/{id}/people',
+        method: 'GET'
+      },
+      params: params,
+      requiredParams: ['slug', 'id'],
+      pathParams: ['slug', 'id'],
+      context: self
+    };
+
+    return createAPIRequest(parameters, callback);
+  };
+}
+
+/**
+ * Inherit from Api.
+ */
+util.inherits(Lists, Api);
+
+/**
+ * Exports Lists object
+ * @type Lists
+ */
+module.exports = Lists;
--- apis/people/v1.js
+++ apis/people/v1.js
@@ -1,31 +1,55 @@
-var createAPIRequest = require('../../lib/apirequest');
-function People(options) {
-  var self = this;
-  this._options = options || {};
-  /**
-   * people.list
-   * @param  {object}   params   Parameters for the request
-   * @param  {callback} callback The callback that handles the response
-   * @return {object}            Request object
-   */
-  this.list = function(params, callback) {
-    var parameters = {
-      options: {
-        url: 'https://{slug}.nationbuilder.com/api/v1/people',
-        method: 'GET'
-      },
-      params: params,
-      requiredParams: ['slug'],
-      pathParams: ['slug'],
-      context: self
-    };
+var Api = require('../../lib/api'),
+	createAPIRequest = require('../../lib/apirequest');
+var util = require('util');
 
-    return createAPIRequest(parameters, callback);
-  }
+function People(options) {
+	People.super_.call(this);
+	var self = this;
+	this._options = options || {};
+	/**
+	 * people.list
+	 * @param  {object}   params   Parameters for the request
+	 * @param  {callback} callback The callback that handles the response
+	 * @return {object}            Request object
+	 */
+	this.list = function(params, callback) {
+		var parameters = {
+			options: {
+				url: 'https://{slug}.nationbuilder.com/api/v1/people',
+				method: 'GET'
+			},
+			params: params,
+			requiredParams: ['slug'],
+			pathParams: ['slug'],
+			context: self
+		};
+
+		return createAPIRequest(parameters, callback);
+	};
+
+	this.show = function(params, callback) {
+		var parameters = {
+			options: {
+				url: 'https://{slug}.nationbuilder.com/api/v1/people/{id}',
+				method: 'GET'
+			},
+			params: params,
+			requiredParams: ['slug', 'id'],
+			pathParams: ['slug', 'id'],
+			context: self
+		};
+
+		return createAPIRequest(parameters, callback);
+	}
 }
 
 /**
-* Exports People object
-* @type People
-*/
+ * Inherit from Api.
+ */
+util.inherits(Lists, Api);
+
+/**
+ * Exports People object
+ * @type People
+ */
 module.exports = People;

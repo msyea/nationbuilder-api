6b9cade9f47be49ab67ad016d45996d706cd4d0e
Simon Mayes <simon@msyea.co.uk>
2015-01-28 15:56:38 +0000

    Fixing inherit

--- apis/people/v1.js
+++ apis/people/v1.js
@@ -1,52 +1,52 @@
 var Api = require('../../lib/api'),
-	createAPIRequest = require('../../lib/apirequest');
+  createAPIRequest = require('../../lib/apirequest');
 var util = require('util');
 
 function People(options) {
-	People.super_.call(this);
-	var self = this;
-	this._options = options || {};
-	/**
-	 * people.list
-	 * @param  {object}   params   Parameters for the request
-	 * @param  {callback} callback The callback that handles the response
-	 * @return {object}            Request object
-	 */
-	this.list = function(params, callback) {
-		var parameters = {
-			options: {
-				url: 'https://{slug}.nationbuilder.com/api/v1/people',
-				method: 'GET'
-			},
-			params: params,
-			requiredParams: ['slug'],
-			pathParams: ['slug'],
-			context: self
-		};
+  People.super_.call(this);
+  var self = this;
+  this._options = options || {};
+  /**
+   * people.list
+   * @param  {object}   params   Parameters for the request
+   * @param  {callback} callback The callback that handles the response
+   * @return {object}            Request object
+   */
+  this.list = function(params, callback) {
+    var parameters = {
+      options: {
+        url: 'https://{slug}.nationbuilder.com/api/v1/people',
+        method: 'GET'
+      },
+      params: params,
+      requiredParams: ['slug'],
+      pathParams: ['slug'],
+      context: self
+    };
 
-		return createAPIRequest(parameters, callback);
-	};
+    return createAPIRequest(parameters, callback);
+  };
 
-	this.show = function(params, callback) {
-		var parameters = {
-			options: {
-				url: 'https://{slug}.nationbuilder.com/api/v1/people/{id}',
-				method: 'GET'
-			},
-			params: params,
-			requiredParams: ['slug', 'id'],
-			pathParams: ['slug', 'id'],
-			context: self
-		};
+  this.show = function(params, callback) {
+    var parameters = {
+      options: {
+        url: 'https://{slug}.nationbuilder.com/api/v1/people/{id}',
+        method: 'GET'
+      },
+      params: params,
+      requiredParams: ['slug', 'id'],
+      pathParams: ['slug', 'id'],
+      context: self
+    };
 
-		return createAPIRequest(parameters, callback);
-	}
+    return createAPIRequest(parameters, callback);
+  }
 }
 
 /**
  * Inherit from Api.
  */
-util.inherits(Lists, Api);
+util.inherits(People, Api);
 
 /**
  * Exports People object
